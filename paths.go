4df08987cc5c6d390307406be42efac4189cdbc6
Alexey Pankiv <a@pankif.com>
2022-07-16 22:41:28 +0300

    init

---
+++ paths.go
@@ -0,0 +1,31 @@
+package paths
+
+import (
+	"errors"
+	"os"
+)
+
+// SplitByGroups returns separated (by os.PathSeparator) string with given length of each group.
+//
+// Split string "abcdefg" to "\a\b\c\d\e\f" for groupsLengths [1,1,1,1,1,1]
+//
+// Split string "abcdefg" to "\a\bc\de\f" for groupsLengths [1,2,2,1]
+//
+// Split string "abcdefg" to "\a\bc" for groupsLengths [1,2]
+func SplitByGroups(str string, groupsLengths ...int) (string, error) {
+	split := ""
+	position := 0
+	separator := string(os.PathSeparator)
+	strLength := len(str)
+
+	for _, length := range groupsLengths {
+		if length+position > strLength {
+			return "", errors.New("groups lengths sum greater then str length")
+		}
+
+		split += separator + str[position:length+position]
+		position += length
+	}
+
+	return split, nil
+}
